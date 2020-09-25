901ce9dea7c2e46fd29368d46d359f04928e00c1
Max Stoiber <contact@mxstbr.com>
2020-09-25 22:01:44 +0200

    Fix author

--- svn/trunk/index.php
+++ svn/trunk/index.php
@@ -5,8 +5,8 @@
    description: >-
   Collect issues, ideas and compliments for your website with a beautiful widget.
    Version: 1.0
-   Author: Max Stoiber
-   Author URI: https://mxstbr.com
+   Author: Feedback Fish
+   Author URI: http://feedback.fish
    License: MIT
    */
 
